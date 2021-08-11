c8e16f16b5a9a6ed4ae529eaaa9bd8f514061029
Maiara <mayaramellovon@yahoo.com.br>
2021-08-10 20:54:45 -0400

    Conclusão SnapCrackle

---
+++ snapcrackle.js
@@ -0,0 +1,21 @@
+function snapCrackle(maxValue) {
+    let retorno = []
+    for(let i = 1; i <= maxValue; i++){
+        if(i % 2 === 1 && i % 5 === 0){
+            retorno.push("SnapCrackle")
+        } 
+        else if(i % 2 === 1){
+            retorno.push("Snap, ")
+        }    
+        else if(i % 5 === 0){
+            retorno.push("Crackle, ")
+        }        
+        else if(i % 2 === 0){
+            retorno.push(i)
+        }
+
+        }
+    
+    return retorno
+}
+console.log(snapCrackle(maxValue))
